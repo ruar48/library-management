49ad0f794af2af4a7f0ce6c2a88aa8fea3aebdbe
Frogerall <saliticoadrian@gmail.com>
2023-11-30 14:27:16 +0800

    remove bootstrap link

--- admin/manage-books.php
+++ admin/manage-books.php
@@ -1,8 +1,6 @@
 <?php include "header/header.php"; ?>
 <?php include "nav/nav.php"; ?>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <div id="layoutSidenav_content">
   <main>
     <div class="container-fluid px-4">
